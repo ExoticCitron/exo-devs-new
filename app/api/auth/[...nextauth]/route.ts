6674cfd7ffd10768f8069cb0cbdd89994bf010e2
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-11-16 15:01:37 +0000

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,27 +1,5 @@
 import NextAuth from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
-
-export const authOptions = {
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
-      authorization: { params: { scope: 'identify email guilds' } },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: { session: any, token: any }) {
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-};
+import { authOptions } from './options';
 
 const handler = NextAuth(authOptions);
 
