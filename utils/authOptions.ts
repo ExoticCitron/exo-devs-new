75126417b2a307f67a6be2a8e9ebef959f615151
Exo <127617294+ExoticCitron@users.noreply.github.com>
2024-11-16 17:34:33 +0000

    Create authOptions.ts

---
+++ utils/authOptions.ts
@@ -0,0 +1,32 @@
+import { NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: 'identify email guilds',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: { session: any, token: any }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
